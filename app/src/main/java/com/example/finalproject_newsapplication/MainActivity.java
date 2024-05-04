a8d1541bbfdba744029c756e5bc3f396f11a94d0
Navneet Mishra <nav47neet@gmail.com>
2024-05-04 12:14:45 +0530

    Files changed

---
+++ app/src/main/java/com/example/finalproject_newsapplication/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.finalproject_newsapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.graphics.Color;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RectShape;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import com.example.finalproject_newsapplication.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bnView;
+
+    FrameLayout fLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bnView = findViewById(R.id.nav_bar);
+        bnView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.nav_home) {
+                    loadFragment(new HomeFragment(), false);
+                } else if (id == R.id.nav_region) {
+                    loadFragment(new RegionFragment(), false);
+                } else {
+                    loadFragment(new CategoryFragment(), true);
+                }
+
+
+                return true;
+            }
+        }); bnView.setSelectedItemId(R.id.nav_home);
+
+    }
+
+    public void loadFragment(Fragment fragment, boolean flag) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container, fragment);
+        fragmentTransaction.commit();
+    }
+}
